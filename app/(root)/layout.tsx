e5073e8361e6d6b2093d71de726e9e821de582a5
nathaniaria <nathan.iaria7@gmail.com>
2024-10-17 13:14:52 +1100

    components & home page

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,16 @@
+import Sidebar from "@/components/ui/Sidebar";
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+
+  const loggedIn = { firstName: "Nathan", lastName: "Fernandes" };
     
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn} />
         {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import HeaderBox from "@/components/ui/HeaderBox";
+import TotalBalanceBox from "@/components/ui/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const logggedIn = { firstName: "Nathan" };
 
-export default Home
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome to Horizon"
+            user={logggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions effieciently"
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
