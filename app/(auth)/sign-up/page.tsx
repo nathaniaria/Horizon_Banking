a8d98627d231e340615c235edaf0ead0b62ab3c6
nathaniaria <nathan.iaria7@gmail.com>
2024-11-15 21:35:56 +1100

    add authentication forms and utility components

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+'use client'
 
-const SignUp = () => {
+import AuthForm from '../../../components/ui/AuthForm'
+
+const SignUp = async () => {
   return (
-    <div>SignUp</div>
+    <section className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-up" />
+    </section>
   )
 }
 
