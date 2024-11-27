730e127abae69e247d7e3619d5cccb3771b077de
nathaniaria <nathan.iaria7@gmail.com>
2024-11-27 18:17:35 +1100

    user actions file created for signIn and signUp

---
+++ lib/actions/user.actions.ts
@@ -0,0 +1,20 @@
+'use server'
+
+export const signIn = async () => {
+    try {
+        // Mutation / Database / Make Fetch
+    } catch
+    (error) {
+        console.error(error)
+    }
+}
+
+
+export const signUp = async (userData: SignUpParams) => {
+    try {
+        // Create user acc 
+    } catch
+    (error) {
+        console.error(error)
+    }
+}
